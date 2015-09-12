024a83f93a9e7cdb81f8dfe39a759d9e21a93e2d
xian0831 <xian0831@gmail.com>
2015-09-12 08:03:08 -0400

    Move declaration of pizzasDiv out of loop.

--- Dev/views/js/main.js
+++ Dev/views/js/main.js
@@ -457,8 +457,8 @@ resizePizzas = function (size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
